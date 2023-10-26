74f6d6c2d802d2329ecb668fb657f7624a21fa1e
paulius-mongirdas <paulius.mongirdas@ktu.edu>
2023-10-26 13:19:34 +0300

    Added editor base

--- Assets/Scripts/CreateObject.cs
+++ Assets/Scripts/CreateObject.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class CreateObject : MonoBehaviour
 {
     [SerializeField] private GameObject[] objectPrefabs;
     private int selectedPrefabIndex = -1;
+    private int objectIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +21,34 @@ public class CreateObject : MonoBehaviour
                 ? objectPrefabs[selectedPrefabIndex] : null;
         }
     }
-    // Update is called once per frame
+    // Only objects created by this function will be saved to json
     public void Create()
     {
         if (SelectedPrefab != null)
-            Instantiate(SelectedPrefab, this.transform.position, this.transform.rotation);
+        {
+            GameObject obj = Instantiate(SelectedPrefab, this.transform.position, this.transform.rotation);
+            string prefabPath = AssetDatabase.GetAssetPath(SelectedPrefab);
+
+            Debug.Log("Prefab path: " + prefabPath);
+            if (ObjectPrefabPaths.paths.ContainsKey(obj.name))
+            {
+                obj.name = obj.name + objectIndex;
+                objectIndex++;
+                ObjectPrefabPaths.paths.Add(obj.name, prefabPath);
+            }
+            else
+            {
+                ObjectPrefabPaths.paths.Add(obj.name, prefabPath);
+            }
+        }
     }
     public void SelectPrefab(int index)
     {
         selectedPrefabIndex = index;
     }
+}
+
+public static class ObjectPrefabPaths
+{
+    public static Dictionary<string, string> paths = new Dictionary<string, string>(); // key - object_name, value - prefab_path
 }
